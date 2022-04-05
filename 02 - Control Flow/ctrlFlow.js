8a72ef417a8c82677ec8e12a3cfc5cdd801ec03c
Duncan James <djduncanjames@hotmail.com>
2022-04-05 21:58:57 +0100

    CTRL Flow 101

---
+++ 02 - Control Flow/ctrlFlow.js
@@ -0,0 +1,16 @@
+//  Control Flow:
+
+// Loops and Conditional Statements
+
+//  Use Loops for cycling through data
+//  Use conditional statements for checking conditons
+                        
+                        // if(some conditon is true) {
+                        //   do something...
+                        // }
+
+
+// FOR Loops
+
+for ( let i=0; i<5; i++){
+}
