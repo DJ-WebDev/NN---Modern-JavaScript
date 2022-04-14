e6ce5fb9cd6a9cbcfc95afbb4e3fa1b67474a0f7
Duncan <djduncanjames@hotmail.com>
2022-04-14 16:39:32 +0100

    Change HTML

--- 06 - Document Object Model/03-changeText.js
+++ 06 - Document Object Model/03-changeText.js
@@ -35,19 +35,3 @@ Lets use the query selector to select a <P> element, and change the text inside 
  // This is how we can change the text
 //---------------------------------------------------------------
 
-// How do we change the actual HTML code inside the DOM?
-//const content = document.querySelector(" .content");
-
-//console.log(content.innerHTML) //= "<h2>THIS IS A NEW H2</h2>";
-// --> What if we want to update it?
-//content.innerHTML = "<h2>THIS IS A NEW H2</h2>"; 
-//  += to append  = to OVERWRITE 
-//-------------------------------------------------------------------------
-
-// lets create an HTML template for each person in a database (Array)
-const content = document.querySelector(" .content");
-const people = ["Mario", "Luigi", "Yoshi"]; // database
-                                            // We want to cycle thru the database and output an HTML template for each person
- people.forEach(person => {   // we can do this with a Callback Function
-   content.innerHTML += `<p>${person}</p>`; // Updates the HTML in the browser
- });
