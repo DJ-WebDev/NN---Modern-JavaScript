a78ba84bc7b1b666ea10e72c452e58d073507554
Duncan <djduncanjames@hotmail.com>
2022-04-14 11:32:55 +0100

    cleanup from laptop

--- 06 - Document Object Model/02- getElement.js
+++ 06 - Document Object Model/02- getElement.js
@@ -6,13 +6,13 @@
 const title = document.getElementById("page-title");
   console.log(title);                //name of id required
 
-// Get elements by their class name
+/* Get elements by their class name
 
-// we named the object errors because we are grabbing 2 error classes (can declare anything)
-const errors =  document.getElementsByClassName("error");
+we named the object errors because we are grabbing 2 error classes (can declare anything)  */
+const errors =  document.getElementsByClassName("error");                                 
   console.log(errors)         //  multiple elements are logged as an HTML Collection 
   console.log(errors[0]) // Like a NODE list, we can use Square Notation to gety a single element from the collection
-/*  logs an HTML Collection   -- We cannot use forEach method on 
+ /* logs an HTML Collection   -- We cannot use forEach method on 
 --> similar but not identical
 
   Get elements by their tag name  */
